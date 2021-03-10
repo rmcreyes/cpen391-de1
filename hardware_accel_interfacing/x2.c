c31535beb240727b6d3cd78e5da9b9937e1aead5
andreamah <andrea_mah22@hotmail.com>
2021-03-09 23:50:28 -0600

    adding test accel file

---
+++ hardware_accel_interfacing/x2.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+
+int init_accel(int* input, int in_size, int l1_size, int l2_size, int l3_size)
+{
+    printf("%d ", in_size);
+    printf("%d ", l1_size);
+    printf("%d ", l2_size);
+    printf("%d ", l3_size);
+}
