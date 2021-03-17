f4009ecb2d610b16619f2a40b7a9c53c92e2f8d3
andreamah <andrea_mah22@hotmail.com>
2021-03-17 19:29:31 +0000

    hw accel files

--- hardware_accel_interfacing/load_from_bin.py
+++ hardware_accel_interfacing/load_from_bin.py
@@ -1,5 +1,14 @@
 import numpy as np
 from ctypes import *
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file")
+args = parser.parse_args()
+
+if args.file is None:
+    print("no file provided")
+    quit()
 
 bin_arr = np.fromfile('6_3.bin', dtype='int32')
 print(bin_arr)
@@ -9,8 +18,8 @@ p = (c_int*len(bin_arr))()
 for idx,elem in enumerate(bin_arr):
     p[idx] = elem.item()
 
-x = CDLL('./x.so')
-x.read.argtype = POINTER(c_int)
-x.read.restype = None
+x = CDLL('./libnn.so')
+x.run.argtype = POINTER(c_int)
+x.run.restype = c_int
 
-x.read(p)
+print(x.run(p))
--- hardware_accel_interfacing/load_nn_to_c.py
+++ hardware_accel_interfacing/load_nn_to_c.py
@@ -15,8 +15,8 @@ p = (c_int*num_elems)()
 for idx,elem in enumerate(bin_arr):
     p[idx] = elem.item()
 
-x = CDLL('./x.so')
-x.read.argtype = POINTER(c_int),c_int,c_int,c_int,c_int
-x.read.restype = None
+x = CDLL('./libnn.so')
+x.init_accel.argtype = POINTER(c_int),c_int,c_int,c_int,c_int
+x.init_accel.restype = c_int
 
-x.read(p)
+print(x.init_accel(p,in_size,l1_size,l2_size,l3_size))
