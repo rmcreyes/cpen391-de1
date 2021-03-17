738737337cf84710f26a1ddee21c5195d642f1df
andreamah <andrea_mah22@hotmail.com>
2021-03-17 13:41:34 -0600

    edited load_nn_files

--- hardware_accel_interfacing/load_nn_to_c.py
+++ hardware_accel_interfacing/load_nn_to_c.py
@@ -20,3 +20,16 @@ x.init_accel.argtype = POINTER(c_int),c_int,c_int,c_int,c_int
 x.init_accel.restype = c_int
 
 print(x.init_accel(p,in_size,l1_size,l2_size,l3_size))
+
+bin_arr = np.fromfile('6_3.bin', dtype='int32')
+# print(bin_arr)
+
+p = (c_int*len(bin_arr))()
+
+for idx,elem in enumerate(bin_arr):
+    p[idx] = elem.item()
+
+x.run.argtype = POINTER(c_int)
+x.run.restype = c_int
+
+print(x.run(p))
