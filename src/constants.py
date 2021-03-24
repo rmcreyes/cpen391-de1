bccfbecaa0a31af624aa44c80b95c2730ab8b30d
andreamah <andrea_mah22@hotmail.com>
2021-03-23 23:27:51 -0600

    updated constants

--- src/constants.py
+++ src/constants.py
@@ -9,7 +9,7 @@ GEN_EXTRACTED_LETTER_PNG = False # whether to always generate letters extracted 
 DEBUG = False # when true, intermittent photos will pop up with letter extraction progress
 SAVE_DEBUG = False # whether to save demo photos
 
-PROMPT_CHECKER = True # whether or not to compare result to given value ahead of time
+PROMPT_CHECKER = False # whether or not to compare result to given value ahead of time
 SAVE_ORIGINALS = True # whether or not to save the original photos taken to a directory
 
 # sizes for resized photos 
@@ -24,7 +24,7 @@ L2_SIZE = 750
 L3_SIZE = 36
 
 NN_BIN = 'models/model_hl_1000_750_generated_nn.bin' # neural network weights and biases (bin format)
-NN_H5 = "models/letterrecog1000_750.h5" # neural network weights and biases (h5 format)
+NN_H5 = "models/model_hl_1000_750.h5" # neural network weights and biases (h5 format)
 SO_FILE = "./src/compiled_c/libnn.so"
 
 # mapping labels to actual character
