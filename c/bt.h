c411c819bcf38cd4bef5e2a44beca9b2cb4fb4cf
Kevin Zhang <shunhanzhang@hotmail.com>
2021-04-06 15:43:15 -0700

    fix header

--- c/bt.h
+++ c/bt.h
@@ -4,5 +4,5 @@ void sendBTLine(char * str);
 int confirm_BT(char * plate, char * buf, int bufSize);
 int ok_done(char * plate, char * buf, int bufSize);
 int ok_leave();
-int ok_user(char * plate, char * buf, int bufSize);
+int ok_user(char * plate, char * buf, int bufSize, int isUser);
 #endif
