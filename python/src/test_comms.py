577ff9ce36f1d168ff24d873de3f9b80022c5c0b
andreamah <andrea_mah22@hotmail.com>
2021-04-06 17:46:25 -0600

    added test file

---
+++ python/src/test_comms.py
@@ -0,0 +1,5 @@
+import c_comm_interfacing_utils
+
+print(c_comm_interfacing_utils.init_rfs_wifi())
+print(c_comm_interfacing_utils.begin_park("ABC 123"))
+print(c_comm_interfacing_utils.confirm_bluetooth("ABC 123"))
